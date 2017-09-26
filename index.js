acbf662a55acb3f1b565a63deb27f65343f79328
Justin Guerin <justin.guerin@keurig.com>
2017-09-26 17:20:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var height = "74"
 if (name === "Susan"){
   console.log("This is correct!")
 }
-
+else{
+  console.log("Excpected the name to be Susan")
+}
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
-
